bad5dd5f1e3ea91c5867b04c1e9ac544ae69e951
Lorenzo <Loremente@outlook.it>
2022-02-05 22:40:47 +0100

    Main class

---
+++ src/main/java/me/loremente/punishtool/PunishTool.java
@@ -0,0 +1,11 @@
+package me.loremente.punishtool;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PunishTool extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+}
